65da4976c8e1f8bb8c3b4f9eb51e583610adad74
DAMA - Max-Emilien Dauvert-Latre <m.dauvert-latre@octo.com>
2021-03-08 15:15:03 +0100

    Reproduce example

--- libraries/ansible/echo.groovy
+++ libraries/ansible/echo.groovy
@@ -1,11 +1,3 @@
 void call(){
-    stages{
-        stage('Pipeline Prerequisites') {
-            steps {
-                script {
-                    sh(script: "echo SUCCESS", returnStdout: true).trim()
-                }
-            }
-        }
-    }
+    println "Success"
 }
